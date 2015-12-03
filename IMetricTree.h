1016ba21c637ace866c5890810bf76eddfec22fe
Seth Wiesman <sjw3kf@mail.missouri.edu>
2015-12-03 13:00:00 -0600

    staging

---
+++ IMetricTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Seth Wiesman on 11/30/15.
+//
+
+#ifndef THESIS_IMETRICTREE_H
+#define THESIS_IMETRICTREE_H
+
+
+class IMetricTree {
+
+};
+
+
+#endif //THESIS_IMETRICTREE_H
---
+++ RangeMetricTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Seth Wiesman on 11/29/15.
+//
+
+#ifndef THESIS_RANGEMETRICTREE_H
+#define THESIS_RANGEMETRICTREE_H
+
+
+class RangeMetricTree {
+
+};
+
+
+#endif //THESIS_RANGEMETRICTREE_H
